3c7c134295b585337a162aaaeb647c5f30601d22
ctmohale <60016492+ctmohale@users.noreply.github.com>
2023-08-11 22:04:34 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sign-translation-system',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
